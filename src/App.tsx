c3f7a64bfbed28a665205d391cbfa130ce6a5343
hjk7 <hjk7@sfu.ca>
2018-04-02 00:40:29 -0700

    package json update for tslint and native base. tsconfig update

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,25 @@ import * as React from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
+import { Button, Text  } from 'native-base';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menuszzz?',
+    'Shake or press menu button for dev menus?',
 });
 
 type Props = {};
 export default class App extends React.Component<Props> {
+
+  componentWillMount(){
+    console.log("test test test?");
+    console.log("tesx"); 
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -34,6 +40,9 @@ export default class App extends React.Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Button>
+            <Text>test button </Text>
+        </Button>        
       </View>
     );
   }
