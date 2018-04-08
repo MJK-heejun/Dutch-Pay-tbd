46ede3e978fabd4898b7257b05ad0f4dce806330
hjk7 <hjk7@sfu.ca>
2018-04-08 16:50:22 -0700

    realm added

---
+++ src/components/service/Storage.tsx
@@ -0,0 +1,11 @@
+const Realm = require('realm');
+//import React, { Component } from 'react';
+
+// export default class Storage extends React.Component{
+export default class Storage {    
+    constructor() { }
+
+    public GetMonthData(year: number, month: number) { //date
+
+    }
+};
