453105f3365e16483cf5811a0780f30442df5bc6
Warren <wlwanpan@uwaterloo.ca>
2018-04-28 11:48:18 -0700

    testing

--- src/service/Storage.tsx
+++ src/service/Storage.tsx
@@ -2,48 +2,48 @@ const Realm = require('realm');
 import { IDateDto, IHistoryDto } from 'dto'
 import { HistoryEnum, PersonEnum } from 'enum'
 
-export default class Storage {    
+export default class Storage {
     private static _instance:Storage = new Storage();
     private realm:any = null;
 
-    constructor() { 
+    constructor() {
     }
 
     public static GetInstance():Storage{
         if (!this._instance) {
             this._instance = new Storage();
         }
-            
+
         return this._instance;
     }
 
     public GetHistoryByYearMonth(year: number, month: number):IHistoryDto[] { //date
-        let result:IHistoryDto[] = this.realm.objects('History').filtered(`year = ${year} AND month = ${month}`); 
-        return result;        
+        let result:IHistoryDto[] = this.realm.objects('History').filtered(`year = ${year} AND month = ${month}`);
+        return result;
     }
 
     public WriteHistory(history:IHistoryDto) {
         this.realm.create('History', history);
-    }    
+    }
 
     private instantiateRealm(callBack:Function) : void {
         Realm.open({
             schema: [{
-                name: 'History', 
+                name: 'History',
                 properties: {
                     id: 'int',
                     year: 'int',
                     month: 'int',
                     day: 'int',
                     amount: 'float',
-                    description: 'string',                    
-                    history_type: 'int', //enum deposit, expense                    
+                    description: 'string',
+                    history_type: 'int', //enum deposit, expense
                     person_type: 'int' //enum host, partner
                 }
             }]
-          }).then((realm:any) => {            
+          }).then((realm:any) => {
             this.realm = realm;
             callBack();
-          });  
+          });
     }
 };
