f115825ca20f32c6c9ee5d3def2e96daba8e42b2
unknown <juna77k@gmail.com>
2018-04-28 16:02:52 -0700

    push agin

--- src/components/common/Calendar.tsx
+++ src/components/common/Calendar.tsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import { ICalendarProps } from 'dto/ICalendarProps';
+import { DayEnum } from 'enum'
+import { ICalendarDto, IHistoryDto, IDateDto } from '../../dto';
+import { FakeStorage } from '../../fake';
 
 export default class Calendar extends React.Component<ICalendarProps, any> {
 
@@ -11,7 +14,24 @@ export default class Calendar extends React.Component<ICalendarProps, any> {
   constructor(props:any){
     super(props);
     console.log(this.props.calendar);
-    console.log(this.props.firstDay); //indicates the first day of the month. (e.g. Tuesday is the first day of 2018 May)
+    console.log(this.props.firstDay); //indicates the first day of the month. (e.g. Tuesday is the first day of 2018 May)    
+    
+    this.state = {
+      tmpCalendar: this.getTmpCalendarObj(),
+      tmpFirstday: DayEnum.wednesday
+    };
+  }
+
+  getTmpCalendarObj():ICalendarDto{
+    let tmpCalendar:ICalendarDto = {};
+    let tmpHistories:IHistoryDto[] = FakeStorage.GetHistoriesByDate(null);
+    let currentDate:IDateDto = {
+      year: new Date().getFullYear(),
+      month: new Date().getMonth(),
+      day: 1
+    };     
+    tmpCalendar[JSON.stringify(currentDate)] = tmpHistories;   
+    return tmpCalendar; 
   }
 
   render() {
