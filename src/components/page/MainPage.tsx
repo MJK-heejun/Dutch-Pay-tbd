7e3ab4bb2f72f0201b9deca6427bd47655d5705e
unknown <juna77k@gmail.com>
2018-04-28 15:50:45 -0700

    redux tmp remove

--- src/components/page/MainPage.tsx
+++ src/components/page/MainPage.tsx
@@ -13,35 +13,18 @@ import { IDateDto, IHistoryDto, ICalendarDto } from 'dto'
 import { getHistoriesByDate } from '../../actions';
 import { DayEnum } from '../../enum';
 
-class MainPage extends React.Component<DispatchProp<any>, any> {
+
+export default class MainPage extends React.Component {
 
   constructor(props:any) {
     super(props);
-
-    //to be removed
-    console.log("this props....");
-    console.log(props);
-
-    let tmpCalendar:ICalendarDto = this.getTmpData();
-    this.state = {
-      tmpCalendar: tmpCalendar
-    };
   }
 
-  componentWillMount(){    
-    //this.initFakeCalendar();
-    //this.readStorageTest();
-    let currentDate:IDateDto = {
-      year: new Date().getFullYear(),
-      month: new Date().getMonth(),
-      day: 1
-    };
-    //this.props.getHistoriesByDate(currentDate); //need to fix this later....
-  }
+  componentWillMount(){ }
 
 
   render() {
-    let calendar:ICalendarDto = this.state.tmpCalendar;
+    let calendar:ICalendarDto = null;
     let tmpDay:DayEnum = DayEnum.thursday;
     return (
       <View>
@@ -51,59 +34,6 @@ class MainPage extends React.Component<DispatchProp<any>, any> {
     )
   }
 
-  //to be removed
-  getTmpData(){
-    let currentDate:IDateDto = {
-      year: new Date().getFullYear(),
-      month: new Date().getMonth(),
-      day: 1
-    };     
-    let tmpCalendar:ICalendarDto = {};
-    let dateKey = JSON.stringify(currentDate);
-    let tmpHistories:IHistoryDto[] = FakeStorage.GetHistoriesByDate(null);
-    tmpCalendar[dateKey] = tmpHistories;
-    currentDate.day = 25;
-    dateKey = JSON.stringify(currentDate);
-    tmpCalendar[dateKey] = tmpHistories;
-    return tmpCalendar;
-  }
-
-  //to be removed
-  initFakeCalendar(){
-    let histories:IHistoryDto[] = FakeStorage.GetHistoriesByDate(null);
-    let date:IDateDto = {
-      year: new Date().getFullYear(),
-      month: new Date().getMonth(),
-      day: 1
-    }; 
-    for (let i = 1; i < 28; i++) { 
-      date.day = i;
-      FakeStorage.SetHistories(date, histories);
-    }    
-    console.log("successfully added!!!");
-  }
-  //to be removed
-  readStorageTest(){
-    let date:IDateDto = {
-      year: new Date().getFullYear(),
-      month: new Date().getMonth(),
-      day: 1
-    };    
-    console.log("will read storage");
-    Storage.GetHistories(date).then((res)=>{
-      console.log(res);
-    }, (err)=>{
-      console.log(err);
-    });
-  }
-
 };
 
 
-const mapStateToProps:any = (state:any) => {
-  //state = all the state objects returned from reducer index
-  let calendar:ICalendarDto = state.calendarSummary;
-  return { calendar: calendar };  
-};
-
-export default connect<any, any, void>(mapStateToProps, {getHistoriesByDate})(MainPage);
